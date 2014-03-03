b0e6001f63e0542059ef8eab53e781dc4f6da5fe
Robert Feldt <robert.feldt@gmail.com>
2014-03-03 09:14:35 +0100

    calc how many times feldts ruby gems has been downloaded

---
+++ src/calc_feldt_rubygems_downloads.jl
@@ -0,0 +1,30 @@
+using Homebrew
+
+# First time there is an error. Works the 2nd time though. Not sure why, but 
+# seems to be a problem with GnuTLS.
+try
+  using GnuTLS
+catch
+end
+
+using GnuTLS
+
+using Requests
+
+r = get("http://rubygems.org/profiles/feldt")
+
+m = match(r"<div><strong>([0-9,]+)</strong> all time</div>", r.data)
+
+if m == nothing
+
+  println("ERROR! Could not identify a dl count!")
+  exit(-1)
+
+else
+
+  count = replace(m.captures[1], ",", "")
+  num_dls = int(count)
+  rounded = int( floor(num_dls/1000) * 1000 )
+  print("Feldt's Ruby gems has been downloaded more than $(rounded) times ($(num_dls))")
+
+end
