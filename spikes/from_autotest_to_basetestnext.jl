e7cc639e7ac4fc76574fb4227185b7813347f76b
Robert Feldt <robert.feldt@gmail.com>
2016-05-28 11:00:03 +0800

    fixed bugs and added some conversions

--- spikes/from_autotest_to_basetestnext.jl
+++ spikes/from_autotest_to_basetestnext.jl
@@ -2,21 +2,39 @@ testfile = ARGS[1]
 
 totab4(str) = " " ^ (div(length(str), 2) * 4)
 
-newlines = open(testfile, "r") do fh
-    map(readlines(fh)) do line
-        m1 = match(r"(\s*)describe(\"(.*)\") do", line)
-        m2 = match(r"(\s*)test(\"(.*)\") do", line)
-        if m1 != nothing
-            "$(totab4(m1.captures[1]))@testset \"$(m1.captures[2])\" begin"
-        elseif m2 != nothing
-            "$(totab4(m2.captures[1]))@testset \"$(m2.captures[2])\" begin"
-        else
-            line
-        end
+function transform_line(line::AbstractString)
+    m = match(r"^(\s*)describe\(\"(.*)\"\)\s+do(.*)\n", line)
+    if m != nothing
+        return "$(totab4(m.captures[1]))@testset \"$(m.captures[2])\" begin$(m.captures[3])\n"
     end
 
+    m = match(r"^(\s*)test\(\"(.*)\"\)\s+do(.*)\n", line)
+    if m != nothing
+        return "$(totab4(m.captures[1]))@testset \"$(m.captures[2])\" begin$(m.captures[3])\n"
+    end
+
+    m = match(r"^(\s*)@repeat\s+test\(\"(.*)\"\)\s+do(.*)\n", line)
+    if m != nothing
+        return "$(totab4(m.captures[1]))@testset \"$(m.captures[2])\" for i in 1:NumReps $(m.captures[3])\n"
+    end
+
+    m = match(r"^(\s*)@check(\s+)(.+)\n", line)
+    if m != nothing
+        return "$(totab4(m.captures[1]))@test$(m.captures[2])$(m.captures[3])\n"
+    end
+
+    m = match(r"^(\s*)([^\s].*)\n", line)
+    if m != nothing
+        return "$(totab4(m.captures[1]))$(m.captures[2])\n"
+    end
+    
+    line
+end
+
+newlines = open(testfile, "r") do fh
+    map(transform_line, readlines(fh))
 end
 
 open(testfile * ".alt", "w") do fh
-    println(fh, join(newlines, "\n"))
+    println(fh, join(newlines, ""))
 end
