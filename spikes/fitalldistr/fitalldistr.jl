3491c850b0ccfa20b6429d2b70fe9e1935d845cd
Robert Feldt <robert.feldt@gmail.com>
2016-08-18 12:12:43 +0200

    fitting all possible distributions

---
+++ spikes/fitalldistr/fitalldistr.jl
@@ -0,0 +1,65 @@
+# The normal fit api in Distributions.jl is:
+#   fit(Distr, values)
+# so we want to do a more general
+#   fitall(values)
+# that will try all distributions that can be fitted and return back the "best" one
+# for relevant criteria of "best", typically BIC or AIC.
+
+using Distributions
+
+AllContinuousDistributions = [
+    Arcsine, Beta, BetaPrime, Cauchy, Chi, Chisq, Erlang, Exponential,
+    FDist, Frechet, Gamma, GeneralizedExtremeValue, GeneralizedPareto,
+    Gumbel, InverseGamma, InverseGaussian, Laplace, Levy, LogNormal,
+    Logistic, Normal, NormalInverseGaussian, Pareto, Rayleigh, 
+    SymTriangularDist, TDist, TriangularDist, Uniform, VonMises, Weibull
+]
+
+ContinuousDistributionsThatCanFit = filter(AllContinuousDistributions) do D
+    try
+        fit_mle(D, samples)
+        true
+    catch _err
+        false
+    end
+end
+
+# Start for continuous distributions
+function fitalldistr{T<:Real}(x::AbstractArray{T}, sortby = :BIC)
+    n = length(x)
+    allfits = map(ContinuousDistributionsThatCanFit) do D
+        d = fit_mle(D, x)
+        loglik = loglikelihood(d, samples)
+        k = length(params(d))
+        result = (d, 0.0)
+        if sortby == :BIC
+            criteria = -2 * loglik + k * log(n)
+        elseif sortby == :AIC
+            criteria = -2 * loglik + 2 * k
+        elseif sortby == :AICc
+            criteria = -2 * loglik + 2 * k + ((2*k*(k+1))/(n-k-1))
+        elseif sortby == :NLogL
+            criteria = -loglik
+        end
+        (d, criteria)
+    end
+    sort(allfits, by = t -> t[2])
+end
+
+function fitall{T<:Real}(x::AbstractArray{T}, sortby = :BIC)
+    fitalldistr(x, sortby)[1][1]
+end
+
+samples1 = rand(Normal(3.4, 1.2), 100)
+d1 = fitall(samples1)
+
+samples2 = rand(Exponential(0.3), 100)
+d2 = fitall(samples2)
+
+samples3 = rand(LogNormal(-5.0, 2.0), 100)
+d3 = fitalldistr(samples3)
+
+samples4 = rand(Gamma(6.20, 1.3), 10)
+d4 = fitalldistr(samples4)
+
+# Should we maybe use a non-parametric test to select between them?
