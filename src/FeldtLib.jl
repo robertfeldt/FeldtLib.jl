9415cb7ba2d39e9482e546635bad1744c9914b30
Robert Feldt <robert.feldt@gmail.com>
2016-09-15 12:04:00 +0200

    added fitall based on spike

--- src/FeldtLib.jl
+++ src/FeldtLib.jl
@@ -2,5 +2,6 @@ module FeldtLib
 
 include("ranks.jl")
 include("baumgartner_weis_schindler.jl")
+include("fitall.jl")
 
 end
---
+++ src/fitall.jl
@@ -0,0 +1,42 @@
+using Distributions
+
+const AllContinuousDistributions = [
+    Arcsine, Beta, BetaPrime, Cauchy, Chi, Chisq, Erlang, Exponential,
+    FDist, Frechet, Gamma, GeneralizedExtremeValue, GeneralizedPareto,
+    Gumbel, InverseGamma, InverseGaussian, Laplace, Levy, LogNormal,
+    Logistic, Normal, NormalInverseGaussian, Pareto, Rayleigh, 
+    SymTriangularDist, TDist, TriangularDist, Uniform, VonMises, Weibull
+]
+
+const ContinuousDistributionsThatCanFit = filter(AllContinuousDistributions) do D
+    try
+        fit_mle(D, rand(10))
+        true
+    catch _err
+        false
+    end
+end
+
+function fitalldistr{T<:Real}(x::AbstractArray{T}, sortby = :BIC)
+    n = length(x)
+    allfits = map(ContinuousDistributionsThatCanFit) do D
+        d = fit_mle(D, x)
+        loglik = loglikelihood(d, x)
+        k = length(params(d))
+        if sortby == :BIC
+            criteria = -2 * loglik + k * log(n)
+        elseif sortby == :AIC
+            criteria = -2 * loglik + 2 * k
+        elseif sortby == :AICc
+            criteria = -2 * loglik + 2 * k + ((2*k*(k+1))/(n-k-1))
+        elseif sortby == :NLogL
+            criteria = -loglik
+        end
+        (d, criteria)
+    end
+    sort(allfits, by = t -> t[2])
+end
+
+function fitall{T<:Real}(x::AbstractArray{T}, sortby = :BIC)
+    fitalldistr(x, sortby)[1][1]
+end
