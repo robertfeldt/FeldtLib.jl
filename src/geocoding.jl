e2c0a1a45516899e7c4004defb208f0ba8574880
Robert Feldt <robert.feldt@gmail.com>
2020-04-23 22:55:47 +0200

    added stamen map function

--- src/geocoding.jl
+++ src/geocoding.jl
@@ -1,5 +1,6 @@
 using HTTP
 using JSON
+using RCall
 
 export get_lat_lon, vgregion_lat_lon_bounds
 
@@ -54,4 +55,27 @@ function vgregion_lat_lon_bounds(delta = 0.10)
 
     # Boundingbox for ggmap given as lowerleftlon, lowerleftlat, upperightlon, upperrightlat
     return (minimum(lons)-delta, minimum(lats)-delta, maximum(lons)+delta, maximum(lats)+delta)
+end
+
+# google maptypes: roadmap, terrain, satellite, hybrid
+# osm maptypes: none...
+
+#get_stamenmap(bbox = c(left = -95.80204, bottom = 29.38048, right =
+#-94.92313, top = 30.14344), zoom = 10, maptype = c("terrain",
+#"terrain-background", "terrain-labels", "terrain-lines", "toner",
+#"toner-2010", "toner-2011", "toner-background", "toner-hybrid",
+#"toner-labels", "toner-lines", "toner-lite", "watercolor"),
+#crop = TRUE, messaging = FALSE, urlonly = FALSE,
+#color = c("color", "bw"), force = FALSE, where = tempdir(), ...)
+function make_stamen_map(boundingbox, filename = "t.png"; 
+    maptype = "toner-lite", color="bw")
+    R"library(ggmap)"
+    left, bottom, right, top = boundingbox
+    R"bbox <- c(left = $left, bottom = $bottom, right = $right, top = $top)"
+    R"zoomlevel <- calc_zoom(bbox)"
+    R"mymap <- get_stamenmap(bbox, zoom=zoomlevel, maptype=$maptype, crop=FALSE, color=$color)"
+    R"p <- ggmap(mymap)"
+    R"ggsave($filename, p)"
+    @info "Saved to file $filename"
+    filename
 end
