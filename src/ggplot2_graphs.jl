40fbd4a9fa8cca64a0e9dd9695fa1a21d8504b6a
Robert Feldt <robert.feldt@gmail.com>
2020-04-27 21:13:46 +0200

    added log scaling of x axis

--- src/ggplot2_graphs.jl
+++ src/ggplot2_graphs.jl
@@ -4,7 +4,8 @@ function ggplot2_distribution(values::AbstractVector, filename;
     title = "Distribution",
     xaxistitle = "Cases",
     types = [:histogram, :density],
-    yaxistitle = "Count")
+    yaxistitle = "Count",
+    xlog10 = false)
     values = convert(Vector{Float64}, values)
     R"library(ggplot2)"
     R"library(ggthemes)"
@@ -14,7 +15,8 @@ function ggplot2_distribution(values::AbstractVector, filename;
     R"df[, xaxistitle] <- values"
     @rput yaxistitle
     @rput title
-    R"p <- ggplot(df, aes_string(x = xaxistitle)) + theme_tufte() + xlab(xaxistitle) + ggtitle(title)"
+    extras = xlog10 ? "+ scale_x_log10()" : ""
+    R"p <- ggplot(df, aes_string(x = xaxistitle)) + theme_tufte() + xlab(xaxistitle) + ggtitle(title) $extras"
     basefilename, ext = splitext(filename)
     if in(:histogram, types)
         R"phist <- p + geom_histogram(binwidth=.5, color='black', fill='blue', alpha=0.5) + ylab(yaxistitle)"
